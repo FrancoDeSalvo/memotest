d1240be9c88a725d53fa70ee60a03ada50076538
Franco <desalvo.fa@gmail.com>
2022-08-02 20:23:47 -0300

    first version with numbers

---
+++ js/main.js
@@ -0,0 +1,110 @@
+/* INICIALIZACION DE VARIABLES */
+let flipedCards = 0;
+let card1 = null;
+let card2 = null;
+let firstResult = null;
+let secondResult = null;
+let moves = 0;
+let success = 0;
+let timer = false;
+const INITIAL_TIME = 45;
+let time = INITIAL_TIME;
+let regressiveTime = null;
+
+let showMoves = document.getElementById("moves");
+let showSuccess = document.getElementById("success");
+let showTime = document.getElementById("timer");
+
+/* ARRAY CON NUMEROS ORDENADOS ALEATORIAMENTE */
+let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+numbers = numbers.sort(() => {
+  return Math.random() - 0.5;
+});
+
+// console.log(numbers);
+
+/* TEMPORIZADOR */
+function countTime() {
+  regressiveTime = setInterval(() => {
+    time--;
+    showTime.innerHTML = `Tiempo: ${time} segundos`;
+    if (time == 0) {
+      clearInterval(regressiveTime);
+      lockCards();
+    }
+  }, 1000);
+}
+
+/* BLOQUEAR TARJETAS */
+function lockCards() {
+  for (let i = 0; i <= 15; i++) {
+    let lockedCard = document.getElementById(i);
+    lockedCard.innerHTML = numbers[i];
+    lockedCard.disabled = true;
+  }
+}
+
+/* FUNCION PRINCIPAL */
+function flip(id) {
+  if (!timer) {
+    countTime();
+    timer = true;
+  }
+
+  flipedCards++;
+
+  // console.log(flipedCards);
+
+  if (flipedCards == 1) {
+    /* MOSTRAR PRIMER NUMERO */
+    card1 = document.getElementById(id);
+    firstResult = numbers[id];
+    card1.innerHTML = firstResult;
+
+    /* DESHABILITAR PRIMER BOTON */
+    card1.disabled = true;
+
+    //    firstResult = prueba(id)
+  } else if (flipedCards == 2) {
+    /* MOSTRAR SEGUNDO NUMERO */
+    card2 = document.getElementById(id);
+    secondResult = numbers[id];
+    card2.innerHTML = secondResult;
+
+    /* DESHABILITAR SEGUNDO BOTON */
+    card2.disabled = true;
+
+    // INCREMENTAR MOVIMIENTOS
+    moves++;
+    showMoves.innerHTML = `Movimientos: ${moves}`;
+
+    /* VERIFICAR QUE LOS DOS RESULTADOS SEAN IGUALES */
+    if (firstResult == secondResult) {
+      /* REINICIAMOS A 0 EL CONTADOR DE TARJETAS DESTAPADAS */
+      flipedCards = 0;
+
+      /* AUMENTAMOS ACIERTOS */
+      success++;
+      showSuccess.innerHTML = `Aciertos: ${success}`;
+
+      /* VERIFICAMOS SI EL JUGADOR COMPLETO LOS ACIERTOS */
+      if (success == 8) {
+        clearInterval(regressiveTime);
+        showSuccess.innerHTML = `Aciertos: ${success} ⭐ `;
+        showMoves.innerHTML = `Movimientos: ${moves} 🕹️`;
+        showTime.innerHTML = `Felicidades, solo te demoraste: ${INITIAL_TIME - time} segundos ⌛`;
+      }
+    } else {
+      // EN CASO DE QUE NO SEAN IGUALES, MOSTRAR MOMENTANEAMENTE VALORES Y VOLVER A TAPAR
+      setTimeout(() => {
+        card1.innerHTML = "";
+        card2.innerHTML = "";
+        card1.disabled = false;
+        card2.disabled = false;
+        flipedCards = 0;
+      }, 800);
+    }
+  }
+
+}

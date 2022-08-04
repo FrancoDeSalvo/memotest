ff9e9c3072c482a7b31eb763899784e741020376
leandro <ezefelippe9@gmail.com>
2022-08-04 17:49:34 -0300

    version with images and sounds

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ const INITIAL_TIME = 45;
 let time = INITIAL_TIME;
 let regressiveTime = null;
 
+let winAudio = new Audio('../sounds/win.mp3');
+let loseAudio = new Audio('../sounds/lose.mp3');
+  
+
 let showMoves = document.getElementById("moves");
 let showSuccess = document.getElementById("success");
 let showTime = document.getElementById("timer");
@@ -32,15 +36,16 @@ function countTime() {
     if (time == 0) {
       clearInterval(regressiveTime);
       lockCards();
+      loseAudio.play();
     }
   }, 1000);
 }
 
-/* BLOQUEAR TARJETAS */
+/* BLOQUEAR TARJETAS PARA MOSTRAR TODAS LAS POSICIONES DE LAS IMAGENES   */
 function lockCards() {
   for (let i = 0; i <= 15; i++) {
     let lockedCard = document.getElementById(i);
-    lockedCard.innerHTML = numbers[i];
+    lockedCard.innerHTML = `<img src="../images/${numbers[i]}.png" alt="" >`;
     lockedCard.disabled = true;
   }
 }
@@ -60,7 +65,7 @@ function flip(id) {
     /* MOSTRAR PRIMER NUMERO */
     card1 = document.getElementById(id);
     firstResult = numbers[id];
-    card1.innerHTML = firstResult;
+    card1.innerHTML = `<img src="../images/${firstResult}.png" alt="" >`;
 
     /* DESHABILITAR PRIMER BOTON */
     card1.disabled = true;
@@ -70,7 +75,7 @@ function flip(id) {
     /* MOSTRAR SEGUNDO NUMERO */
     card2 = document.getElementById(id);
     secondResult = numbers[id];
-    card2.innerHTML = secondResult;
+    card2.innerHTML =`<img src="../images/${secondResult}.png" alt="" >`;
 
     /* DESHABILITAR SEGUNDO BOTON */
     card2.disabled = true;
@@ -83,10 +88,11 @@ function flip(id) {
     if (firstResult == secondResult) {
       /* REINICIAMOS A 0 EL CONTADOR DE TARJETAS DESTAPADAS */
       flipedCards = 0;
-
+      
       /* AUMENTAMOS ACIERTOS */
       success++;
       showSuccess.innerHTML = `Aciertos: ${success}`;
+      
 
       /* VERIFICAMOS SI EL JUGADOR COMPLETO LOS ACIERTOS */
       if (success == 8) {
@@ -94,6 +100,7 @@ function flip(id) {
         showSuccess.innerHTML = `Aciertos: ${success} ⭐ `;
         showMoves.innerHTML = `Movimientos: ${moves} 🕹️`;
         showTime.innerHTML = `Felicidades, solo te demoraste: ${INITIAL_TIME - time} segundos ⌛`;
+        winAudio.play()
       }
     } else {
       // EN CASO DE QUE NO SEAN IGUALES, MOSTRAR MOMENTANEAMENTE VALORES Y VOLVER A TAPAR
@@ -106,5 +113,4 @@ function flip(id) {
       }, 800);
     }
   }
-
 }
